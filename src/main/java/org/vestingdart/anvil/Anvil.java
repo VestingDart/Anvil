3c530dbad7cdb6dba20461dec93db7a82f039db9
VestingDart <sasuke.uchiha2010@yahoo.com>
2025-03-17 20:39:53 +0100

    Fixed Anvil Bug and shut work now

--- src/main/java/org/vestingdart/anvil/Anvil.java
+++ src/main/java/org/vestingdart/anvil/Anvil.java
@@ -1,9 +1,13 @@
 package org.vestingdart.anvil;
 
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.event.inventory.InventoryType;
+import org.bukkit.inventory.AnvilInventory;
+import org.bukkit.inventory.Inventory;
 
 public class Anvil implements CommandExecutor {
     private Object aNull;
@@ -13,7 +17,7 @@ public class Anvil implements CommandExecutor {
         Player player = (Player) commandSender;
 
         if (player.hasPermission("galaxicraft.multi.anvil")) {
-            player.getInventory(null,true);
+            Inventory anvil = Bukkit.createInventory(null, InventoryType.ANVIL, "Custom Anvil");
 
         } else {
             player.sendMessage("§x§7§C§1§3§F§BG§x§8§4§2§5§F§Ba§x§8§C§3§8§F§Cl§x§9§4§4§A§F§Ca§x§9§C§5§C§F§Dx§x§A§4§6§F§F§Di§x§A§B§8§1§F§DC§x§B§3§9§3§F§Er§x§B§B§A§5§F§Ea§x§C§3§B§8§F§Ff§x§C§B§C§A§F§Ft §8•§4 Du hast dafür keine Rechte.");
