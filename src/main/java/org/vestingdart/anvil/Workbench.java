e16ecf1afa8050c8c12eb30f1b9932be34935772
VestingDart <sasuke.uchiha2010@yahoo.com>
2025-03-23 18:51:51 +0100

    Workbench is working now

--- src/main/java/org/vestingdart/anvil/Workbench.java
+++ src/main/java/org/vestingdart/anvil/Workbench.java
@@ -1,22 +1,27 @@
 package org.vestingdart.anvil;
 
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.event.inventory.InventoryType;
 
 public class Workbench implements CommandExecutor {
     private Object aNull;
 
     @Override
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
+        if (!(commandSender instanceof Player)){
+            return true;
+        }
         Player player = (Player) commandSender;
 
         if (player.hasPermission("galaxicraft.multi.workbench")) {
             player.getInventory();
-            player.openWorkbench()
+            player.openInventory(Bukkit.createInventory(null, InventoryType.WORKBENCH));
         }
 
-        return false;
+        return true;
     }
 }
