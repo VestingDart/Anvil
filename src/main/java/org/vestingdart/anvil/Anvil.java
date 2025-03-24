4e74b33f83d91dd7a1195fd06fd6722387129b73
VestingDart <sasuke.uchiha2010@yahoo.com>
2025-03-24 18:08:57 +0100

    Edited plugin.yml

--- src/main/java/org/vestingdart/anvil/Anvil.java
+++ src/main/java/org/vestingdart/anvil/Anvil.java
@@ -17,9 +17,8 @@ public class Anvil implements CommandExecutor {
         Player player = (Player) commandSender;
 
         if (player.hasPermission("galaxicraft.multi.anvil")) {
-            Inventory anvil = Bukkit.createInventory(null, InventoryType.ANVIL, "Custom Anvil");
-
-
+            player.getInventory();
+            player.openInventory(Bukkit.createInventory(null, InventoryType.ANVIL));
         } else {
             player.sendMessage("§x§7§C§1§3§F§BG§x§8§4§2§5§F§Ba§x§8§C§3§8§F§Cl§x§9§4§4§A§F§Ca§x§9§C§5§C§F§Dx§x§A§4§6§F§F§Di§x§A§B§8§1§F§DC§x§B§3§9§3§F§Er§x§B§B§A§5§F§Ea§x§C§3§B§8§F§Ff§x§C§B§C§A§F§Ft §8•§4 Du hast dafür keine Rechte.");
         }
