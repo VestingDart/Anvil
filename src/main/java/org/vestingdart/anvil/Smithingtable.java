63e58f0a709c0fad34184a9cd60a8f1aa689ce4e
VestingDart <sasuke.uchiha2010@yahoo.com>
2025-03-25 22:21:38 +0100

    Startet working on the Smithingtable

--- src/main/java/org/vestingdart/anvil/Smithingtable.java
+++ src/main/java/org/vestingdart/anvil/Smithingtable.java
@@ -1,4 +1,26 @@
 package org.vestingdart.anvil;
 
-public class Smithingtable {
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.event.inventory.InventoryType;
+
+public class Smithingtable implements CommandExecutor {
+    private Object aNull;
+
+    @Override
+    public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
+        Player player = (Player) commandSender;
+
+        if (player.hasPermission("galaxicraft.multi.smithingtable")) {
+            player.getInventory();
+            player.openInventory(Bukkit.createInventory(null, InventoryType.SMITHING));
+        } else {
+            player.sendMessage("§x§7§C§1§3§F§BG§x§8§4§2§5§F§Ba§x§8§C§3§8§F§Cl§x§9§4§4§A§F§Ca§x§9§C§5§C§F§Dx§x§A§4§6§F§F§Di§x§A§B§8§1§F§DC§x§B§3§9§3§F§Er§x§B§B§A§5§F§Ea§x§C§3§B§8§F§Ff§x§C§B§C§A§F§Ft §8•§4 Du hast dafür keine Rechte.");
+        }
+        return false;
+
+    }
 }
